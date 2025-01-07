2597ad267ae163f74de57746e1ec32506d1cf2b3
이시온 <sion9199@gmail.com>
2025-01-07 09:45:18 +0900

    1

--- day5/memberlist/DBInfo.java
+++
@@ -1,8 +0,0 @@
-package annotation.springmvc.memberlist;
-
-public class DBInfo {
-	static final public String driver = "oracle.jdbc.driver.OracleDriver";
-	static final public String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	static final public String account = "c##jdbc";
-	static final public String password = "jdbc";
-}
